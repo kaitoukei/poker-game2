0988513077a4f78a3fd39f5b1c35d6cfccd59acc
Keiko Fujiyama <f.partner.rev2009@gmail.com>
2019-04-19 12:48:44 +0900

    デッキ作成実装

--- fujifuji-poker.rb
+++ fujifuji-poker.rb
@@ -1,4 +1,15 @@
+require_relative "card"
+
 #=====================
 # fujifuji poker main
 #---------------------
 # 52枚の手札を作る
+deck = []
+p Card::SUITS
+Card::SUITS.each do |k, v|
+    1.upto(13) do |n|
+        deck << Card.new(k, n)
+    end
+end
+
+puts deck
